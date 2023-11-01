226f5ff8436c238e561494eca370a9483a7e72f3
Mickey <mickey_fraser@outlook.com>
2023-11-01 17:53:07 -0500

    Added basic SweveModule code

--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -1,5 +1,35 @@
 package frc.robot;
 
-public class SwerveModule {
+import com.ctre.phoenix.sensors.CANCoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 
+public class SwerveModule {
+    public CANSparkMax turnMotor;
+    public CANSparkMax wheelMotor;
+
+    public CANCoder angleEncoder;
+    public RelativeEncoder turnMotorEncoder;
+    public RelativeEncoder wheelEncoder;
+
+    public SparkMaxPIDController turnPID;
+    public SparkMaxPIDController wheelPID;
+
+    public SwerveModule (int turnID, int wheelID, int encoderID) {
+        turnMotor = new CANSparkMax(turnID, CANSparkMaxLowLevel.MotorType.kBrushless);
+        wheelMotor = new CANSparkMax(wheelID, CANSparkMaxLowLevel.MotorType.kBrushless);
+        turnMotorEncoder = turnMotor.getEncoder();
+        wheelEncoder = wheelMotor.getEncoder();
+        //can set pid values here
+    }
+
+    public void turnToAngle(double angle){
+        turnPID.setReference(angle*12.8, CANSparkMax.ControlType.kPosition);
+    }
+
+    public void setSpeed(double speed){
+        wheelPID.setReference(speed, CANSparkMax.ControlType.kVelocity);
+    }
 }
