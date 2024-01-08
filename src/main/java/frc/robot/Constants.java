0e09064ba05ab3c5255407975f92bf04348bb847
Mickey <mickey_fraser@outlook.com>
2024-01-07 18:33:46 -0600

    Tuning values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,6 +22,7 @@ import frc.robot.utils.SwerveModuleConstants;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
+
 public final class Constants {
   public static class kControls {
     public static final double AXIS_DEADZONE = 0.1;
