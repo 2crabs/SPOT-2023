ef5564ab14dbe1c0e73bbc233ac453840885caca
Washburn <the2crabs@gmail.com>
2024-01-23 16:42:48 -0600

    Added Intake Commands

--- src/main/java/frc/robot/commands/StopIntake.java
+++ src/main/java/frc/robot/commands/StopIntake.java
@@ -5,38 +5,25 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants.kManip;
 import frc.robot.subsystems.ManipulatorSubsystem;
 
-public class IntakeNote extends CommandBase {
+public class StopIntake extends CommandBase {
   private final ManipulatorSubsystem manipSubsystem;
 
-  // CHANGE THIS LATER. this boolean should turn true when the note is finished intake. we don't know how to do that yet.
-  private boolean intakeFinished;
-
-  /** Creates a new IntakeNote. */
-  public IntakeNote(ManipulatorSubsystem manipulator) {
-    manipSubsystem = manipulator;
-    addRequirements(manipulator);
+  /** Creates a new StopIntake. */
+  public StopIntake(ManipulatorSubsystem subsystem) {
+    manipSubsystem = subsystem;
+    addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    manipSubsystem.setPredeterminedIntakeMotorAngle(1);
-    intakeFinished = false;
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (!intakeFinished) {
-      manipSubsystem.setPredeterminedIntakeMotorAngle(1);
-    manipSubsystem.setIntakeSpinSpeed(kManip.INTAKE_SPIN_SPEED);
-    } else {
-      manipSubsystem.setPredeterminedIntakeMotorAngle(0);
-      manipSubsystem.setIntakeSpinSpeed(0);
-    }
+    manipSubsystem.setIntakeSpinSpeed(0);
   }
 
   // Called once the command ends or is interrupted.
